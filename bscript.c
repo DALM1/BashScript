43cb72c8c427aa156c62f482984817711fc7ce25
DALM1 <92697916+DALM1@users.noreply.github.com>
2023-02-07 14:59:24 +0100

    Update bscript.c

--- bscript.c
+++ bscript.c
@@ -1,6 +1,6 @@
-Il est déconseillé d'écrire un script pour chiffrer tous les dossiers sur votre ordinateur Mac car cela pourrait causer des problèmes de perte de données ou d'accès non autorisé. Il est préférable d'utiliser des outils de chiffrement de disque intégrés tels que FileVault ou des outils de chiffrement de fichiers tels que VeraCrypt pour protéger vos données sensibles.
+// Il est déconseillé d'écrire un script pour chiffrer tous les dossiers sur votre ordinateur Mac car cela pourrait causer des problèmes de perte de données ou d'accès non autorisé. Il est préférable d'utiliser des outils de chiffrement de disque intégrés tels que FileVault ou des outils de chiffrement de fichiers tels que VeraCrypt pour protéger vos données sensibles.
 
-Si vous insistez pour écrire un script pour chiffrer tous les dossiers sur votre Mac, voici un exemple de script qui utilise OpenSSL pour chiffrer tous les fichiers dans un dossier spécifié en utilisant une clé de chiffrement spécifiée:
+// Si vous insistez pour écrire un script pour chiffrer tous les dossiers sur votre Mac, voici un exemple de script qui utilise OpenSSL pour chiffrer tous les fichiers dans un dossier spécifié en utilisant une clé de chiffrement spécifiée:
 
 
 
@@ -20,17 +20,17 @@ find $folder -type f -exec openssl enc -aes-256-cbc -salt -in {} -out {}.enc -k 
 find $folder -type f -not -name "*.enc" -delete
 
 
-Attention:
+// Attention:
 
-Assurez-vous de remplacer "/path/to/folder" avec le chemin vers le dossier à chiffrer et "password" avec la clé de chiffrement souhaitée.
-Ce script chiffrera TOUS les fichiers dans le dossier spécifié, incluant les fichiers cachés et les sous-dossiers, il est donc important de vous assurer que vous avez une sauvegarde de vos fichiers importants avant de l'utiliser.
-Ce script supprimera les fichiers originaux après le chiffrement, il est donc important de vous assurer que vous avez une sauvegarde de vos fichiers importants avant de l'utiliser.
-Je vous répète encore une fois que cette méthode de chiffrement de dossiers est très dangereuse et peut causer des problèmes de perte de données. Il est préférable d'utiliser des outils de chiffrement de disque intégrés tels que FileVault ou des outils de chiffrement de fichiers tels que VeraCrypt pour protéger vos données sensibles.
+// Assurez-vous de remplacer "/path/to/folder" avec le chemin vers le dossier à chiffrer et "password" avec la clé de chiffrement souhaitée.
+// Ce script chiffrera TOUS les fichiers dans le dossier spécifié, incluant les fichiers cachés et les sous-dossiers, il est donc important de vous assurer que vous avez une sauvegarde de vos fichiers importants avant de l'utiliser.
+// Ce script supprimera les fichiers originaux après le chiffrement, il est donc important de vous assurer que vous avez une sauvegarde de vos fichiers importants avant de l'utiliser.
+// Je vous répète encore une fois que cette méthode de chiffrement de dossiers est très dangereuse et peut causer des problèmes de perte de données. Il est préférable d'utiliser des outils de chiffrement de disque intégrés tels que FileVault ou des outils de chiffrement de fichiers tels que VeraCrypt pour protéger vos données sensibles.
 
 --------------------------------------------------------------
 
 
-Voici un exemple de script en Bash qui parcourt récursivement tous les sous-dossiers à partir d'un dossier de base spécifié:
+// Voici un exemple de script en Bash qui parcourt récursivement tous les sous-dossiers à partir d'un dossier de base spécifié:
 
 Copy code
 #!/bin/bash
@@ -45,16 +45,16 @@ done
 
 
 
-Assurez-vous de remplacer "/path/to/base/folder" avec le chemin vers le dossier de base à parcourir.
-Le script imprimera le chemin complet de chaque fichier trouvé dans le dossier de base.
-Si vous voulez ajouter des opérations sur les fichiers trouvés, vous pouvez les ajouter à l'intérieur de la boucle.
-Il est important de noter que cette boucle parcourra tous les sous-dossiers et sous-fichiers récursivement, il est donc important de vous assurer que vous avez suffisamment d'espace disque disponible et que vous avez les autorisations nécessaires pour accéder aux dossiers.
+// Assurez-vous de remplacer "/path/to/base/folder" avec le chemin vers le dossier de base à parcourir.
+// Le script imprimera le chemin complet de chaque fichier trouvé dans le dossier de base.
+// Si vous voulez ajouter des opérations sur les fichiers trouvés, vous pouvez les ajouter à l'intérieur de la boucle.
+// Il est important de noter que cette boucle parcourra tous les sous-dossiers et sous-fichiers récursivement, il est donc important de vous assurer que vous avez suffisamment d'espace disque disponible et que vous avez les autorisations nécessaires pour accéder aux dossiers.
 
-Il est également important de noter que ce script peut prendre un certain temps pour parcourir tous les dossiers et sous-dossiers d'un ordinateur, il est donc préférable de l'exécuter lorsque vous avez suffisamment de temps libre et que vous pouvez laisser votre ordinateur allumé.
+// Il est également important de noter que ce script peut prendre un certain temps pour parcourir tous les dossiers et sous-dossiers d'un ordinateur, il est donc préférable de l'exécuter lorsque vous avez suffisamment de temps libre et que vous pouvez laisser votre ordinateur allumé.
 
 ---------------------------------------------------------------
 
-Voici un exemple de script en Bash qui permet de changer le fond d'écran d'un bureau Mac:
+// Voici un exemple de script en Bash qui permet de changer le fond d'écran d'un bureau Mac:
 
 #!/bin/bash
 
@@ -67,7 +67,7 @@ osascript -e "tell application \"Finder\" to set desktop picture to POSIX file \
 ------------------------------------------------------------------------
 
 
-Voici un exemple de script en Bash qui ouvre une fenêtre en boucle:
+// Voici un exemple de script en Bash qui ouvre une fenêtre en boucle:
 
 #!/bin/bash
 
@@ -80,14 +80,14 @@ for i in $(seq 1 $num_windows); do
 done
 
 
-Assurez-vous de remplacer "Application Name" avec le nom de l'application pour laquelle vous voulez ouvrir une fenêtre.
-Assurez-vous de remplacer "num_windows" avec le nombre de fenêtre que vous voulez ouvrir.
-Ce script utilise la commande open pour ouvrir une fenêtre de l'application spécifiée.
-Il est important de noter que cela peut causer des problèmes pour votre ordinateur, comme par exemple, un ralentissement ou une utilisation excessive de la mémoire vive, et il est donc important d'utiliser ce script avec précaution. Il est également important de noter que cela peut causer des problèmes pour l'application ou l'ordinateur,
+// Assurez-vous de remplacer "Application Name" avec le nom de l'application pour laquelle vous voulez ouvrir une fenêtre.
+// Assurez-vous de remplacer "num_windows" avec le nombre de fenêtre que vous voulez ouvrir.
+// Ce script utilise la commande open pour ouvrir une fenêtre de l'application spécifiée.
+// Il est important de noter que cela peut causer des problèmes pour votre ordinateur, comme par exemple, un ralentissement ou une utilisation excessive de la mémoire vive, et il est donc important d'utiliser ce script avec précaution. Il est également important de noter que cela peut causer des problèmes pour l'application ou l'ordinateur,
 
 ------------------------------------------------------------ 
 
-Voici un exemple de script en Bash qui parcourt récursivement tous les sous-dossiers à partir d'un dossier de base spécifié et renomme les dossiers en ajoutant un préfixe spécifié:
+// Voici un exemple de script en Bash qui parcourt récursivement tous les sous-dossiers à partir d'un dossier de base spécifié et renomme les dossiers en ajoutant un préfixe spécifié:
 
 #!/bin/bash
 
@@ -139,14 +139,14 @@ for dir in $(find $base_folder -type d); do
   mv "$dir" "$(dirname $dir)/$new_name"
 done
 
-Assurez-vous de remplacer "/path/to/base/folder" avec le chemin vers le dossier de base à parcourir.
-Assurez-vous de remplacer "new_prefix_" avec le préfixe que vous souhaitez ajouter aux noms des dossiers.
-Le script parcourera récursivement tous les sous-dossiers à partir du dossier de base spécifié, et renommera les dossiers en ajoutant le préfixe spécifié.
-Il est important de noter que ce script va renommer tous les dossiers, y compris les dossiers système, donc il est important de vous assurer que vous avez les autorisations nécessaires pour accéder aux dossiers et de faire une sauvegarde de vos données avant de l'exécuter. Il est également important de vérifier que le script fonctionne correctement avant de l'exécuter sur des dossiers importants.
+// Assurez-vous de remplacer "/path/to/base/folder" avec le chemin vers le dossier de base à parcourir.
+// Assurez-vous de remplacer "new_prefix_" avec le préfixe que vous souhaitez ajouter aux noms des dossiers.
+// Le script parcourera récursivement tous les sous-dossiers à partir du dossier de base spécifié, et renommera les dossiers en ajoutant le préfixe spécifié.
+// Il est important de noter que ce script va renommer tous les dossiers, y compris les dossiers système, donc il est important de vous assurer que vous avez les autorisations nécessaires pour accéder aux dossiers et de faire une sauvegarde de vos données avant de l'exécuter. Il est également important de vérifier que le script fonctionne correctement avant de l'exécuter sur des dossiers importants.
 
------------------------------------------------------------------------
+// -----------------------------------------------------------------------
 
-Voici un exemple de script en Bash qui rassemble tous les dossiers d'un ordinateur dans un seul dossier:
+// Voici un exemple de script en Bash qui rassemble tous les dossiers d'un ordinateur dans un seul dossier:
 
 #!/bin/bash
 
@@ -160,6 +160,6 @@ for dir in $(find / -type d); do
 done
 
 
-Assurez-vous de remplacer "/path/to/destination/folder" avec le chemin vers le dossier de destination où vous souhaitez rassembler tous les dossiers de l'ordinateur.
-Le script parcourera récursivement tous les dossiers à partir de la racine de l'ordinateur et copiera tous les dossiers dans le dossier de destination spécifié.
-Il est important de noter que cela peut prendre un certain temps pour parcourir tous les dossiers de l'ordinateur et pour copier tous les fichiers, il est donc préférable de l'exécuter lorsque vous avez suffisamment de temps libre et que vous pouvez laisser votre ordinateur allumé. Il est également important de vérifier que vous avez suffisamment d'espace disque disponible pour copier tous les dossiers dans le dossier de destination. Il est important de faire une sauvegarde de vos données avant de l'exécuter.
+// Assurez-vous de remplacer "/path/to/destination/folder" avec le chemin vers le dossier de destination où vous souhaitez rassembler tous les dossiers de l'ordinateur.
+// Le script parcourera récursivement tous les dossiers à partir de la racine de l'ordinateur et copiera tous les dossiers dans le dossier de destination spécifié.
+// Il est important de noter que cela peut prendre un certain temps pour parcourir tous les dossiers de l'ordinateur et pour copier tous les fichiers, il est donc préférable de l'exécuter lorsque vous avez suffisamment de temps libre et que vous pouvez laisser votre ordinateur allumé. Il est également important de vérifier que vous avez suffisamment d'espace disque disponible pour copier tous les dossiers dans le dossier de destination. Il est important de faire une sauvegarde de vos données avant de l'exécuter.
